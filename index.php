a6591983c4099f497be281aa26764ede942af106
Marcus Absher <mabsher@mail.greenriver.edu>
2019-01-07 20:34:03 -0500

    created css

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="cupcake.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
@@ -86,7 +87,7 @@
                 echo '<label><input class="checkbox" type="checkbox" name="flavors[]" ';
                 if(isset($_POST["flavors"])){if(in_array($key, $_POST["flavors"])){
                     echo  'checked="checked"'; }};
-                echo ' value="'.$key.'">'.$value.'</label>';
+                echo ' value="'.$key.'">  '.$value.'</label>';
                 echo '<br>';
             }
         ?>
@@ -100,6 +101,7 @@
                 if($formOk){
                     $total = sizeof($_POST["flavors"])*3.50;
                     $total = money_format('%.2n', $total);
+                    echo '<br><br>';
                     echo 'Thank you, '.$name.', for your order!';
                     echo '<br><br>';
                     echo '<p>Order Summary:</p>';
